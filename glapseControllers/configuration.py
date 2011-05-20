754e15a767f87b1a9592120769fe6590deaecad3
siondream <david.saltares@gmail.com>
2011-05-20 12:20:14 +0000

    Reverts glapse version

--- glapseControllers/configuration.py
+++ glapseControllers/configuration.py
@@ -6,7 +6,7 @@ import os
 class Configuration:
 	
 	def __init__(self):
-		self.version = 0.2
+		self.version = 0.1
 		#self.dataDir = '/usr/share/glapse/data'
 		#self.libDir = '/usr/lib/glapse'
 		#self.binPath = '/usr/bin/glapse'
