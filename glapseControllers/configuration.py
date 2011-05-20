5c835a05c375ac6ee3ca96322f7a35e6a82f5cff
siondream <david.saltares@gmail.com>
2011-05-20 08:50:30 +0000

    Adds German translation by ChrSpangler

--- glapseControllers/configuration.py
+++ glapseControllers/configuration.py
@@ -6,16 +6,16 @@ import os
 class Configuration:
 	
 	def __init__(self):
-		self.version = 0.1
-		self.dataDir = '/usr/share/glapse/data'
-		self.libDir = '/usr/lib/glapse'
-		self.binPath = '/usr/bin/glapse'
-		self.langDir = '/usr/share/locale'
+		self.version = 0.2
+		#self.dataDir = '/usr/share/glapse/data'
+		#self.libDir = '/usr/lib/glapse'
+		#self.binPath = '/usr/bin/glapse'
+		#self.langDir = '/usr/share/locale'
 		
-		#self.dataDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../data'))
-		#self.libDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../glapseControllers'))
-		#self.binPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '../'))
-		#self.langDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../lang'))
+		self.dataDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../data'))
+		self.libDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../glapseControllers'))
+		self.binPath = os.path.realpath(os.path.join(os.path.dirname(__file__), '../'))
+		self.langDir = os.path.realpath(os.path.join(os.path.dirname(__file__), '../lang'))
 		
 	def getVersion(self):
 		return self.version
